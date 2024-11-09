c61b7e4c9af35cc88c6ced4c022e7e5e1609d569
Alex <git@pentive.com>
2024-11-08 19:11:53 -0600

    add file input and tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("setInputFiles", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("#selectafile").click();
+  await page.locator("#selectafile").setInputFiles("playwright.config.ts");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("filechooser", async ({ page }) => {
+  await page.goto("/");
+  const fileChooserPromise = page.waitForEvent("filechooser");
+  await page.locator("#selectafile").click();
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles("playwright.config.ts");
 });
